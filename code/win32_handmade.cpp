896b7363d031191f02a975d74ef4bf5d697893c7
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-25 19:35:21 +1100

    remove owndc flag

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -151,7 +151,7 @@ WinMain(HINSTANCE Instance,
 {
     WNDCLASSA WindowClass = {};
 
-    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.style = CS_HREDRAW | CS_VREDRAW;
     WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
