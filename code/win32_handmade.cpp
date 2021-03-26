d725b63634ff6a2236fe95c4d4f064625c87ae84
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-27 09:34:54 +1100

    get gamepad

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <xinput.h>
 #include <stdint.h>
 
 #define internal static
@@ -201,6 +202,40 @@ WinMain(HINSTANCE Instance,
             TranslateMessage(&Message);
             DispatchMessageA(&Message);
         };
+
+        for (DWORD ControllerIndex = 0;
+             ControllerIndex < XUSER_MAX_COUNT;
+             ++ControllerIndex)
+        {
+            XINPUT_STATE ControllerState;
+            if (XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS)
+            {
+                XINPUT_GAMEPAD *Pad = &ControllerState.Gamepad;
+
+                bool Up = Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+                bool Down = Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+                bool Left = Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+                bool Right = Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+
+                bool Back = Pad->wButtons & XINPUT_GAMEPAD_BACK;
+                bool Start = Pad->wButtons & XINPUT_GAMEPAD_START;
+
+                bool LeftShoulder = Pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+                bool RightShoulder = Pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+
+                bool AButton = Pad->wButtons & XINPUT_GAMEPAD_A;
+                bool BButton = Pad->wButtons & XINPUT_GAMEPAD_B;
+                bool XButton = Pad->wButtons & XINPUT_GAMEPAD_X;
+                bool YButton = Pad->wButtons & XINPUT_GAMEPAD_Y;
+
+                int16 StickX = Pad->sThumbLX;
+                int16 StickY = Pad->sThumbLY;
+            }
+            else
+            {
+            }
+        }
+
         RenderWeirdGradient(GlobalBackBuffer, XOffset++, YOffset++);
 
         HDC DeviceContext = GetDC(Window);
