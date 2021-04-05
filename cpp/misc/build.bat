079474d73ad48a586d8069acc755645969c47e36
Arseniy Khvorov <khvorov45@gmail.com>
2021-04-05 12:56:03 +1000

    disable optimisations

--- cpp/misc/build.bat
+++ cpp/misc/build.bat
@@ -2,5 +2,5 @@
 
 mkdir build
 pushd build
-cl -MT -nologo -GR- -Gm- -EHa- -Oi -WX -W4 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map ..\code\win32_handmade.cpp /link -opt:ref user32.lib gdi32.lib
+cl -MT -nologo -GR- -Gm- -EHa- -Od -Oi -WX -W4 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map ..\code\win32_handmade.cpp /link -opt:ref user32.lib gdi32.lib
 popd
