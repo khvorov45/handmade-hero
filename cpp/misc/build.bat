4354c8cbc2edf28f0685287af469b4dc409779ea
Arseniy Khvorov <khvorov45@gmail.com>
2021-04-24 07:08:06 +1000

    debug version of runtime library

--- cpp/misc/build.bat
+++ cpp/misc/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set CommonLinkerFlags=-opt:ref user32.lib gdi32.lib winmm.lib -incremental:no
-set CommonCompilerFlags=-MT -nologo -GR- -Gm- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
+set CommonCompilerFlags=-MTd -nologo -GR- -Gm- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
 
 mkdir build
 pushd build
