68c6d0d802b5504282184dca32b8a53ad62d9f41
Arseniy Khvorov <khvorov45@gmail.com>
2021-04-30 14:18:40 +1000

    add game library compile

--- misc/build-linux.sh
+++ misc/build-linux.sh
@@ -1,5 +1,6 @@
 mkdir -p build
 
+clang -shared -fpic -g -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_LINUX=1 -Wno-null-dereference code/game/lib.cpp -o build/game_lib.so
 clang -g -lX11 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_LINUX=1 -Wno-null-dereference code/linux/main.cpp -o build/linux_main
 
 echo Done
