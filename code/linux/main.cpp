cb97bec22a34b4dfd747f9faa6f2994a2b603433
Arseniy Khvorov <khvorov45@gmail.com>
2021-05-03 08:23:41 +1000

    get the game displaying in the buffer

--- code/linux/main.cpp
+++ code/linux/main.cpp
@@ -12,11 +12,22 @@ int main() {
 
     GameCode game_code = load_game_code();
 
+    thread_context thread = {};
+
+    game_memory game_memory = {};
+    game_memory.PermanentStorageSize = Megabytes(64);
+    game_memory.PermanentStorage = malloc(game_memory.PermanentStorageSize);
+
+    game_input game_input = {};
+
     for (;;) {
         XEvent event;
         XNextEvent(window.display, &event); // XCheckWindowEvent doesn't block
 
-        draw(graphics_buffer.game_buffer.Memory, width, height);
+        game_code.game_update_and_render(
+            &thread, &game_memory, &game_input, &graphics_buffer.game_buffer
+        );
+
         display_x11_graphics_buffer(&graphics_buffer, &window);
     }
     return 0;
