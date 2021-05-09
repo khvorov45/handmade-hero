bd1e987a046c718d4b4e44d57ffb2be2daccc2c9
Arseniy Khvorov <khvorov45@gmail.com>
2021-05-09 10:25:28 +1000

    use masks for bmp loading

--- code/game/bmp.cpp
+++ code/game/bmp.cpp
@@ -28,6 +28,22 @@ struct bitmap_header {
 };
 #pragma pack(pop)
 
+struct bit_scan_result {
+    bool32 Found;
+    uint32 Index;
+};
+
+internal bit_scan_result FindLeastSignificantSetBit(uint32 Value) {
+    bit_scan_result Result = {};
+    for (uint32 Test = 0; Test < 32; ++Test) {
+        if (Value & (1 << Test)) {
+            Result.Index = Test;
+            Result.Found = true;
+            break;
+        }
+    }
+    return Result;
+}
 
 struct loaded_bmp {
     uint32* Pixels;
@@ -48,22 +64,36 @@ internal loaded_bmp DEBUGLoadBMP(
     bitmap_header* Header = (bitmap_header*)(ReadResult.Contents);
     Result.Height = Header->Height;
     Result.Width = Header->Width;
-    /*
-    * Byte order is BB GG RR AA
-    * Loaded as 0xAARRGGBB which is what we expect
-    * Except not really because some files are actually AA BB GG RR, so the format is per-file
-    * apparently.
-    */
+
+    uint32 RedMask = Header->RedMask;
+    uint32 GreenMask = Header->GreenMask;
+    uint32 BlueMask = Header->BlueMask;
+    uint32 AlphaMask = ~(RedMask | GreenMask | BlueMask);
+
+    bit_scan_result RedResult = FindLeastSignificantSetBit(RedMask);
+    bit_scan_result GreenResult = FindLeastSignificantSetBit(GreenMask);
+    bit_scan_result BlueResult = FindLeastSignificantSetBit(BlueMask);
+    bit_scan_result AlphaResult = FindLeastSignificantSetBit(AlphaMask);
+
+    Assert(RedResult.Found);
+    Assert(GreenResult.Found);
+    Assert(BlueResult.Found);
+    Assert(AlphaResult.Found);
+
+    uint32 RedShift = RedResult.Index;
+    uint32 GreenShift = GreenResult.Index;
+    uint32 BlueShift = BlueResult.Index;
+    uint32 AlphaShift = AlphaResult.Index;
+
     uint32* Pixels = (uint32*)((uint8*)ReadResult.Contents + Header->BitmapOffset);
     uint32* Pixel = Pixels;
-    uint32 AlphaMask = ~(Header->RedMask | Header->GreenMask | Header->BlueMask);
     for (int32 Y = 0; Y < Header->Height; ++Y) {
         for (int32 X = 0; X < Header->Width; ++X) {
-            uint32 Red = (*Pixel & Header->RedMask);
-            uint32 Green = (*Pixel & Header->GreenMask);
-            uint32 Blue = (*Pixel & Header->BlueMask);
-            uint32 Alpha = (*Pixel & AlphaMask);
-            *Pixel++ = (*Pixel >> 8) | (*Pixel << 24);
+            *Pixel++ =
+                (((*Pixel >> AlphaShift) & 0xFF) << 24) |
+                (((*Pixel >> RedShift) & 0xFF) << 16) |
+                (((*Pixel >> GreenShift) & 0xFF) << 8) |
+                (((*Pixel >> BlueShift) & 0xFF));
         }
     }
     Result.Pixels = Pixels;
