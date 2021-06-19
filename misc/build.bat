4edaa7337b9d263a1d397a3be4a524bbfda574d5
Arseniy Khvorov <khvorov45@gmail.com>
2021-06-19 10:39:51 +1000

    add done message

--- misc/build.bat
+++ misc/build.bat
@@ -9,3 +9,5 @@ del *.pdb
 cl %CommonCompilerFlags% ..\code\game\lib.cpp -Fmgame_lib.map -Fegame_lib.dll -Fogame_lib.obj  -LD /link /EXPORT:GameUpdateAndRender /EXPORT:GameGetSoundSamples -incremental:no -PDB:win32_lib_%random%.pdb
 cl %CommonCompilerFlags% ..\code\win32\main.cpp -Fmwin32_handmade.map -Fewin32_main.exe -Fowin32_main.obj /link %CommonLinkerFlags%
 popd
+
+echo Done
